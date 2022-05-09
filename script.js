2ba8820e2fee9c74943c0e5dbf8486d1684d1af4
Wren Sanchez <demisa00@gmail.com>
2022-05-08 17:31:25 -0700

    Began writing function for password generation and naming variables.

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-// Assignment Code
+// Variables
 var generateBtn = document.querySelector("#generate");
+var password = "";
+
+function generatePassword () {
+var passwordLength = prompt("Enter desired password length (must be between 8 and 100 characters)");
+if (passwordLength < 8 || > 100) {
+  alert("Invalid password length.")
+}
+var lowCase = confirm("Would you like lowercase letters in your password?");
+var upCase = confirm("Would you like lowercase letters in your password?");
+var num = confirm("Would you like numbers in your password?");
+var symb = confirm("Would you like symbols in your password?");
+if (lowCase === false && upCase === false && num === false && symb === false) {
+  alert("Please choose at least one option to continue.")
+}
+
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
