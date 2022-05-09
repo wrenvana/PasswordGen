46aa21b16c4632b0e77b0983ed64568eee11f4cb
Wren Sanchez <demisa00@gmail.com>
2022-05-08 18:10:29 -0700

    Added for loop to generate password.

--- script.js
+++ script.js
@@ -2,19 +2,37 @@
 var generateBtn = document.querySelector("#generate");
 var password = "";
 
+// Character generator
 function generatePassword () {
 var passwordLength = prompt("Enter desired password length (must be between 8 and 100 characters)");
-if (passwordLength < 8 || > 100) {
+if (passwordLength < 8 || passwordLength > 100) {
   alert("Invalid password length.")
 }
 var lowCase = confirm("Would you like lowercase letters in your password?");
-var upCase = confirm("Would you like lowercase letters in your password?");
+var upCase = confirm("Would you like uppercase letters in your password?");
 var num = confirm("Would you like numbers in your password?");
 var symb = confirm("Would you like symbols in your password?");
 if (lowCase === false && upCase === false && num === false && symb === false) {
-  alert("Please choose at least one option to continue.")
+  alert("Please choose at least one option to continue.") 
 }
-
+  var characters = "";
+  if (lowCase === true) {
+  characters = characters.concat("abcdefghijklmnopqrstuvwxyz")
+  }
+  if (upCase === true) {
+    characters = characters.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  }
+  if (num === true) {
+    characters = characters.concat ("0123456789")
+  }
+  if (symb === true) {
+    characters = characters.concat ("!@#$%^&*()")
+  }
+var charLength = characters.length;
+for (var i = 0; i < passwordLength; i++) {
+  password += characters.charAt(Math.floor(Math.random() * charLength));
+  document.querySelector("#password").setAttribute("placeholder", password)
+}
 }
 
 // Write password to the #password input
